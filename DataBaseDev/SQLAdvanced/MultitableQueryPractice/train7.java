062ff16906440650f1e1cd7cfcbc723f95f579b3
tewentong <44704147+tewentong@users.noreply.github.com>
2021-03-26 14:34:04 +0800

    Add files via upload

--- DataBaseDev/SQLAdvanced/MultitableQueryPractice/train7.java
+++ DataBaseDev/SQLAdvanced/MultitableQueryPractice/train7.java
@@ -28,6 +28,18 @@
                       LEFT OUTER JOIN EMP boss ON e.mgr=boss.empno
                       LEFT OUTER JOIN SALGRADE s ON e.sal BETWEEN s.LOSAL AND s.HISAL
             WHERE e.sal>(SELECT AVG(sal) FROM EMP);
+            程序输出：
+                +-------+-------+-----------+------+------------+---------+------+--------+------------+-----------+-----------+
+                | EMPNO | ENAME | JOB       | MGR  | HIREDATE   | SAL     | COMM | DEPTNO | dname      | boss_name | sal_grade |
+                +-------+-------+-----------+------+------------+---------+------+--------+------------+-----------+-----------+
+                |  7566 | JONES | MANAGER   | 7839 | 1981-04-02 | 2975.00 | NULL |     20 | RESEARCH   | KING      |         4 |
+                |  7698 | BLAKE | MANAGER   | 7839 | 1981-05-01 | 2850.00 | NULL |     30 | SALES      | KING      |         4 |
+                |  7782 | CLARK | MANAGER   | 7839 | 1981-06-09 | 2450.00 | NULL |     10 | ACCOUNTING | KING      |         4 |
+                |  7788 | SCOTT | ANALYST   | 7566 | 1987-04-19 | 3000.00 | NULL |     20 | RESEARCH   | JONES     |         4 |
+                |  7839 | KING  | PRESIDENT | NULL | 1981-11-17 | 5000.00 | NULL |     10 | ACCOUNTING | NULL      |         5 |
+                |  7902 | FORD  | ANALYST   | 7566 | 1981-12-03 | 3000.00 | NULL |     20 | RESEARCH   | JONES     |         4 |
+                +-------+-------+-----------+------+------------+---------+------+--------+------------+-----------+-----------+
+
 */
 public class train7 {
 
