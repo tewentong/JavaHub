c12f6ba532b3cebaac672b808172c0dbde79caf5
tewentong <44704147+tewentong@users.noreply.github.com>
2021-04-06 16:17:40 +0800

    Add files via upload

--- WebFoundation/Servlet/TomcatServer.java
+++ WebFoundation/Servlet/TomcatServer.java
@@ -10,6 +10,17 @@
             当在一台机器上配置好Apache服务器，可利用它响应HTML页面的访问请求。
                 实际上Tomcat部分是Apache服务器的扩展，但它是独立运行的，所以当你运行Tomcat时，它作为一个与Apache独立的进程单独运行
             当配置正确时，Apache为HTML页面服务，而Tomcat实际上是在运行JSP页面和Servlet
+    安装Tomcat：
+        Tomcat默认占用端口8080，注意端口冲突问题
+        如果需要使用服务器，启动成功后，该启动窗口不要关闭
+        bin:启动和关闭tomcat的bat文件
+        conf:配置文件server.xml该文件用于配置server相关的信息，比如tomcat启动的端口号，配置主机(host)；
+            web.xml文件配置与web应用（web应用相当于一个web站点）；
+            tomcat-user.xml配置用户名密码和相关的权限
+        lib:该目录放置运行tomcat运行需要的jar包
+        log:存放日志，当我们需要查看日志的时候，可以查询信息
+        webapps:放置我们的web应用
+        work工作目录：该目录用于存放jsp被访问后生成对应的server文件和.class文件
 */
 public class TomcatServer {
 
